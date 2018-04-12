10fa477ef65241e2c0e447a744b31ed3549b9e4b
cfeitong <cfeitong@gmail.com>
2018-04-12 21:36:58 +0800

    tune

--- ecs/predict.cpp
+++ ecs/predict.cpp
@@ -43,11 +43,10 @@ void predict_server(char *info[MAX_INFO_NUM], char *data[MAX_DATA_NUM],
         auto &s = samples[flavor];
         lr->init(n, s);
         lr->train(2000, 1e-4, 1e-3);
-//        double ans0 = lr->predict(pred);
+        double ans0 = lr->predict(pred);
         double ans1 = KalmanPred(pred);
         // get flavor id
-//        int dd = int((ans0+ans1)/2+0.5);
-        int dd = (int)(ans1+0.5);
+        int dd = int(sqrt(ans0*ans1)+0.5);
         flavornum[get_flavor_id(flavor) - 1] = dd;
         for (int i = 0; i < dd; i++) {
             alloc.add_elem(flavor);
