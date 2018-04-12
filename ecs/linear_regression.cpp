46b82aef0aec92c134edb8a58530ae112767e380
cfeitong <cfeitong@gmail.com>
2018-04-12 19:36:11 +0800

    fix

--- ecs/linear_regression.cpp
+++ ecs/linear_regression.cpp
@@ -49,7 +49,8 @@ pdvd LinearRegression::loss(double reg) {
         l += reg * this->w[i] * this->w[i];
     for (int i = 0; i < n; i++) {
         grad[i] /= this->trainset.size();
-        double importance = exp(-(c - i % c - 1) / (2 * k * k));
+//        double importance = exp(-(c - i % c - 1) / (2 * k * k));
+        double importance = 1;
         grad[i] += 2 * reg * this->w[i] * importance;
     }
     return pdvd(l, grad);
