2fc971bab379db03fe029b9079a29b676ef81932
cfeitong <cfeitong@gmail.com>
2018-04-13 19:31:03 +0800

    change days per block

--- ecs/predict.cpp
+++ ecs/predict.cpp
@@ -50,8 +50,8 @@ void predict_server(char *info[MAX_INFO_NUM], char *data[MAX_DATA_NUM],
         double ans1 = KalmanPred(pred);
         double ans3 = pred[pred.size() - 1];
         // get flavor id
-//        int dd = int((ans0 + ans1 + ans3) / 3 + 0.5);
-        int dd = int(ans1 * (meta.days * 1.0 / DAYS_PER_BLOCK) + 0.5);
+        int dd = int((ans0 + ans1 + ans3 * (meta.days * 1.0 / DAYS_PER_BLOCK)) / 3 + 0.5);
+//        int dd = int(ans1 * (meta.days * 1.0 / DAYS_PER_BLOCK) + 0.5);
         flavornum[get_flavor_id(flavor) - 1] = dd;
         for (int i = 0; i < max(dd, 1); i++) {
             alloc.add_elem(flavor);
--- ecs/test/test_alloc.cpp
+++ ecs/test/test_alloc.cpp
@@ -4,7 +4,7 @@
 #include "allocation.h"
 
 #include <string>
-#include <fstream>
+#include <cstdio>
 
 using namespace std;
 
@@ -12,6 +12,15 @@ int main() {
     string base = "data/deploy_test_cases/case";
     for (int i = 1; i <= 6; i++) {
         string path = base + to_string(i) + ".txt";
+        FILE *f = fopen(path.c_str(), "r");
+        int cpu, mem, disk;
+        fscanf(f, "%d%d%d", &cpu, &mem, &disk);
+        char tmp[10]; fscanf(f, "%s", tmp); string opt(tmp);
+        int cnt; fscanf(f, "%d", &cnt);
+        for (int j = 0; j < cnt; j++) {
+            int num; fscanf(f, "%s:%d", tmp, &num);
+            string flavor(tmp);
+        }
     }
 
     return 0;
