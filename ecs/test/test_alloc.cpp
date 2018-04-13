8c2e9a40300572c6dbf9f2d58341ae1c1be44bfa
cfeitong <cfeitong@gmail.com>
2018-04-13 19:26:22 +0800

    change days per block

--- ecs/test/test_alloc.cpp
+++ ecs/test/test_alloc.cpp
@@ -3,8 +3,16 @@
 //
 #include "allocation.h"
 
+#include <string>
+#include <fstream>
+
+using namespace std;
+
 int main() {
     string base = "data/deploy_test_cases/case";
+    for (int i = 1; i <= 6; i++) {
+        string path = base + to_string(i) + ".txt";
+    }
 
     return 0;
 }
