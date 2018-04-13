2415dafd198db5e21e7455c00be5e38744de5aba
cfeitong <cfeitong@gmail.com>
2018-04-13 20:38:24 +0800

    tune

--- ecs/predict.cpp
+++ ecs/predict.cpp
@@ -54,7 +54,7 @@ void predict_server(char *info[MAX_INFO_NUM], char *data[MAX_DATA_NUM],
         double_exponential_smoothing<double, 1> dbexpsmth;
         dbexpsmth.set_1st_smoothing_constant(0.98);
         dbexpsmth.set_2nd_smoothing_constant(0.75);
-        dbexpsmth.set_vacillation_tolerance(10);
+//        dbexpsmth.set_vacillation_tolerance(10);
         es_vec<double, 1> curr_query, smth_result;
         for (auto &i : blockdata) {
             curr_query[0] = i;
@@ -62,9 +62,11 @@ void predict_server(char *info[MAX_INFO_NUM], char *data[MAX_DATA_NUM],
         }
         double ans4 = smth_result[0];
 
+        double ans = (ans0 + ans1 + ans4) / 3;
+
         // get flavor id
 //        int dd = int((ans0 + ans1 + ans3 * (meta.days * 1.0 / DAYS_PER_BLOCK)) / 3 + 0.5);
-        int dd = int(ans4 * (meta.days * 1.0 / DAYS_PER_BLOCK) + 0.5);
+        int dd = int(ans * (meta.days * 1.0 / DAYS_PER_BLOCK) + 0.5);
         for (int i = 0; i < max(dd, 1); i++) {
             alloc.add_elem(flavor);
         }
